89c480ff8cb603eebb694a86f553209421ac5e6a
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-02 23:35:39 +0900

    fargateモジュールの変数整形

--- modules/fargate/variables.tf
+++ modules/fargate/variables.tf
@@ -1,15 +1,55 @@
-variable basename {}
-variable cron {}
-variable cpu {}
-variable memory {}
-variable region {}
-variable image {}
-variable ecs_task_role_arn {}
-variable ecs_event_role_arn {}
-variable command {}
+variable basename {
+  type        = string
+  description = "各リソースに付与される先頭の名前"
+}
+
+variable cron {
+  type        = string
+  description = "バッチのcron式"
+}
+
+variable cpu {
+  default     = "256"
+  type        = string
+  description = "タスクレベルのCPU設定"
+}
+
+variable memory {
+  default     = "512"
+  type        = string
+  description = "タスクレベルのメモリ設定"
+}
+
+variable region {
+  type        = string
+  description = "タスクログのリージョン"
+}
+
+variable image {
+  type        = string
+  description = "イメージURI"
+}
+
+variable ecs_task_role_arn {
+  type        = string
+  description = "タスクロール、アプリケーションが使用する権限を付与するIAMロール"
+}
+variable ecs_event_role_arn {
+  type        = string
+  description = "CloudWatchイベントからECSを起動する権限を持つIAMロール"
+}
+
+variable command {
+  type        = list(string)
+  description = "タスク実行時のコマンド"
+}
+
 variable security_groups {
     type = list(string)
+    description = "タスクに設定されるセキュリティグループ"
 }
+
 variable subnets {
     type = list(string)
+    description = "タスクに設定されるサブネット"
 }
