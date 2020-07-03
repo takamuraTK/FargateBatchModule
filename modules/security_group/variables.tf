2e0693d99ac9ad688052776ccc85a5bfa732ac13
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-03 23:56:16 +0900

    セキュリティグループの変数整形

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -1,6 +1,19 @@
-variable "name" {}
-variable "vpc_id" {}
-variable "port" {}
+variable "name" {
+  type = string
+  description = "セキュリティグループ名"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "セキュリティグループを設定するVPCのID"
+}
+
+variable "port" {
+  type = string
+  description = "インバウンドに設定するポート番号"
+}
+
 variable "cidr_blocks" {
     type = list(string)
+    description = "インバウンドを許可するCIDRブロック"
 }
