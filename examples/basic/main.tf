7edef93bb35fc3f35b7dddbce4fce2a7258e30ce
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-06 23:54:57 +0900

    VPCmodule to repos

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,25 +1,14 @@
-# 条件分岐で必要なときのみにしたい
-resource "aws_vpc" "fargate_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags       = { Name = var.basename }
-}
-
-resource "aws_subnet" "fargate_subnet" {
-  vpc_id     = aws_vpc.fargate_vpc.id
-  cidr_block = "10.0.0.0/24"
-}
-
 module "fargate" {
   basename           = var.basename
-  source             = "./modules/fargate"
+  source             = "../../"
   cron               = "cron(*/2 * * * ? *)"
   cpu                = "256"
   memory             = "512"
   region             = var.region
-  ecs_task_role_arn  = module.ecs_task_role.iam_role_arn
-  ecs_event_role_arn = module.ecs_event_role.iam_role_arn
-  security_groups    = [module.security_group.security_group_id]
-  subnets            = [aws_subnet.fargate_subnet.id]
+  ecs_task_role_arn  = aws_iam_role.fargate_task.arn
+  ecs_event_role_arn = aws_iam_role.fargate_event.arn
+  security_groups    = [aws_security_group.fargate.id]
+  subnets            = [module.vpc.subnet_id]
   container_definitions = jsonencode([
     {
       name      = var.basename
@@ -33,4 +22,11 @@ module "fargate" {
       ]
     }
   ])
+}
+
+module "vpc" {
+  source            = "git::https://github.com/takamuraTK/terraform-aws-vpc.git"
+  basename          = var.basename
+  vpc_cidr_block    = "10.0.0.0/16"
+  subnet_cidr_block = "10.0.0.0/24"
 }
