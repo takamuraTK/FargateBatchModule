6c1cdf4b6564dbdff287ff142b39e61da76ed4cb
takamuraTK <tirpitz.zzz@outlook.jp>
2020-07-08 22:54:03 +0900

    Dataが不要だったので削除

--- iam.tf
+++ iam.tf
@@ -15,16 +15,10 @@ data "aws_iam_policy_document" "fargate_event" {
   }
 }
 
-data "aws_iam_policy" "fargate_event" {
-  name   = "${var.basename}_fargate_event_policy"
-  policy = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
-}
-
 resource "aws_iam_role_policy_attachment" "fargate_event" {
   role       = aws_iam_role.fargate_event.name
-  policy_arn = data.aws_iam_policy.fargate_event.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
 }
-
 
 # ECS task role
 resource "aws_iam_role" "fargate_task" {
@@ -43,12 +37,7 @@ data "aws_iam_policy_document" "fargate_task" {
   }
 }
 
-data "aws_iam_policy" "fargate_task" {
-  name   = "${var.basename}_task_policy"
-  policy = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
-}
-
 resource "aws_iam_role_policy_attachment" "fargate_task" {
   role       = aws_iam_role.fargate_task.name
-  policy_arn = aws_iam_policy.fargate_task.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
 }
